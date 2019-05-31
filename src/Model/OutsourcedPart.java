70c88578bb094a566a37f44486f65e8360be53fc
thwippp <bschaffeld06@gmail.com>
2019-05-31 06:20:14 -0700

    Added SubClasses... still need to figure out this whole extends business.  Also may need to refactor things to take into account the SimpleStringProperty vs. string business.

---
+++ src/Model/OutsourcedPart.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author bscha
+ */
+public class OutsourcedPart extends Part{
+    
+    // Instance variables
+    private String companyName;
+    
+    // NB created this constructor because it didn't like mine
+    public OutsourcedPart(int partId, String partName, double partPrice, int partStock, int partMin, int partMax) {
+        super(partId, partName, partPrice, partStock, partMin, partMax);  // don't know where this comes from
+    }
+    
+    // Setters
+    public void setCompanyName(String companyName){
+        this.companyName = companyName;
+    }
+    
+    // Getters
+    public String getCompanyName(){
+        return this.companyName;
+    }
+    
+}
