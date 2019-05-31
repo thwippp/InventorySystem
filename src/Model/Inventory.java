e1f49d42f66a1defa192e8cb9f716dea7acc677e
thwippp <bschaffeld06@gmail.com>
2019-05-31 06:35:49 -0700

    Added all classes and methods.  Some are commented out because I don't know how to implement ObservableLists yet.  It still compiles!

--- src/Model/Inventory.java
+++ src/Model/Inventory.java
@@ -5,18 +5,94 @@
  */
 package Model;
 
+import javafx.collections.ObservableList;
+
 /**
  *
  * @author bscha
  */
 public class Inventory {
     
-    // placeholder
-    public String a;
+// Do something fancy here
+private ObservableList<Part> allParts;
+private ObservableList<Product> allProducts;
 
-    public Inventory(String a) {
-        this.a = a;
-    }
+
+// Setters
+/*
++ addPart(part:Part):void 
++ addProduct(product:Product):void 
+
++ updatePart(int:index, part:Part):void 
++ updateProduct(int:index, product:Product):void 
+*/
+
+public void addPart(Part part){
+    // do stuff
+}
+
+public void addProduct(Product product){
+    // do stuff
+}
+
+public void updatePart(int index, Part part){
+    // do stuff
+}
+
+public void updateProduct(int index, Product product){
+    // do stuff
+}
+
+
+// Get-Rid-Of-Ers
+/*
++ deletePart(part:Part):void 
++ deleteProduct(product:Product):void
+*/
+
+public void deletePart(Part part){
+    // do stuff
+}
+
+public void deleteProduct(Product product){
+    // do stuff
+}
+
+
+// Getters
+/*
++ lookupPart(partId:int):Part 
++ lookupProduct(productId:int):Product 
++ lookupPart(partName:String):ObservableList<Part>
++ lookupProduct(productName:String):ObservableList<Product>
++ getAllParts():ObservableList<Part>
++ getAllProducts():ObservableList<Product>
+*/
+
+// Commented out because I don't know what to return yet
+//public Part lookupPart(int partId){
+//    //  return Part
+//}
+//
+//public Product looupProduct(int productId){
+//    // return Product
+//}
+//
+//public ObservableList<Part> lookupPart(String partName){
+//    // return Part
+//}
+//
+//public ObservableList<Product> lookupProduct(String productName){
+//    // return Product
+//}
+//
+//public ObservableList<Part> getAllParts(){
+//    // return Parts
+//}
+//
+//public ObservableList<Product> getAllProducts(){
+//    // return Products
+//}
     
     
     
